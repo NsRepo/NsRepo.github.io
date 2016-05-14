bd6da5d9ebcee9a5bffd74a8da5ee8068a8bcbae
NsRepo <NsBm0d@gmail.com>
2016-05-14 18:16:50 -0500

    description

--- gpkg.sh
+++ gpkg.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+find . -name '*.DS_Store' -type f -delete
 chmod +x packages.sh ; chmod +x push.sh ; chmod +x remove.sh ; chmod +x update.sh
--- packages.sh
+++ packages.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 dpkg-deb -bZgzip projects/icons debs
 dpkg-deb -bZgzip projects/NsTheme debs
-dpkg-deb -bZgzip projects/NsB debs
